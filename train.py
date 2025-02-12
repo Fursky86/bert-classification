94024f827d34ee8c9103a45663566ecc82f4ac5f
Fursky86 <jiangxiaoyang_dec@163.com>
2025-02-13 00:08:53 +0100

    Updated train.py and support more parameters

--- train.py
+++ train.py
@@ -6,12 +6,20 @@ from datasets import load_dataset
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
 import evaluate
 
+
+##禁用wandb
+os.environ["WANDB_DISABLED"] = "true"
+
+
+
 # 解析命令行参数
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--model", type=str, default="hfl/chinese-bert-wwm-ext", help="选择预训练模型")
 parser.add_argument("--batch_size", type=int, default=8, help="Batch size")
 parser.add_argument("--epochs", type=int, default=3, help="训练轮数")
+parser.add_argument("--learning_rate", type=float, default=2e-5, help="learning_rate")
+parser.add_argument("--gradient_accumulation_steps", type=int, default=16, help="梯度累积步长")
 args = parser.parse_args()
 
 # 加载 Tokenizer
@@ -36,13 +44,13 @@ training_args = TrainingArguments(
     per_device_train_batch_size=args.batch_size,
     per_device_eval_batch_size=args.batch_size,
     num_train_epochs=args.epochs,
-    evaluation_strategy="epoch",
+    eval_strategy="epoch",
     save_strategy="epoch",
     logging_dir="./logs",
     logging_steps=100,
-    gradient_accumulation_steps=16,
+    gradient_accumulation_steps=args.gradient_accumulation_steps,
     fp16=True,
-    learning_rate=2e-5,
+    learning_rate=args.learning_rate,
 )
 
 # 评估函数
